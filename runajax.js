9c3065d38bbed7ca697e835bffaaef0e598f93ce
nuaaqy <nuaaqy507@163.com>
2019-05-22 16:49:56 +0800

    增加测试代码

---
+++ runajax.js
@@ -0,0 +1,28 @@
+const jsdom = require("jsdom");
+const { JSDOM } = jsdom;
+const { window } = new JSDOM(`<!DOCTYPE html>`);
+const $ = require('jQuery')(window);
+
+const exList = [
+    {"name":"多个用户信息","path":"user.json"},
+    {"name":"单个用户信息","path":"oneuser.json"},
+    {"name":"用户信息列表","path":"users.json"}
+];
+
+
+const runAjax = function(exPath) {
+    console.log("RUN " + exPath.name + ", path: " + exPath.path);
+    $.ajax({
+        url: 'https://raw.githubusercontent.com/nuaaqy/ex/master/'+exPath.path,
+        type: 'get',
+        success: function(rs) {
+            console.log(exPath.name);
+            console.log(rs);    
+        },
+        error: function(a,b,c) {
+            console.log("error");
+        }
+    });
+}
+
+exList.forEach(v => runAjax(v));
